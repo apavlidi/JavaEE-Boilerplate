90d8b371abdc2b50abfe1322cf4516e022166c0d
apavlidi <apavlidi@it.teithe.gr>
2019-05-05 14:29:06 +0300

    Added a batch test

--- src/test/java/com/apavlidi/BasicTest.java
+++ src/test/java/com/apavlidi/BasicTest.java
@@ -1,20 +1,27 @@
 package com.apavlidi;
 
+import static org.junit.Assert.assertEquals;
+
 import com.apavlidi.api.exceptions.DataNotFoundException;
+import com.apavlidi.batch.BatchTestHelper;
+import com.apavlidi.batch.SimpleChunkItemReader;
 import com.apavlidi.domain.Note;
 import com.apavlidi.repository.NoteRepository;
 import com.apavlidi.service.NoteService;
 import com.apavlidi.util.EntityManagerProducer;
 import java.io.File;
+import java.util.Properties;
+import javax.batch.operations.JobOperator;
+import javax.batch.runtime.BatchRuntime;
+import javax.batch.runtime.BatchStatus;
+import javax.batch.runtime.JobExecution;
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
-import org.jboss.shrinkwrap.api.asset.EmptyAsset;
 import org.jboss.shrinkwrap.api.spec.WebArchive;
 import org.jboss.shrinkwrap.resolver.api.maven.Maven;
-import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
@@ -35,9 +42,12 @@ public class BasicTest {
             NoteRepository.class.getPackage(),
             DataNotFoundException.class.getPackage(),
             Note.class.getPackage(),
+            BatchTestHelper.class.getPackage(),
+            SimpleChunkItemReader.class.getPackage(),
             EntityManagerProducer.class.getPackage())
         .addAsResource("META-INF/persistence.xml")
-        .addAsManifestResource(EmptyAsset.INSTANCE, "beans.xml")
+        .addAsResource("META-INF/batch-jobs/simpleChunk.xml")
+        .addAsResource("META-INF/beans.xml")
         .addAsLibraries(pomDependencies);
   }
 
@@ -56,7 +66,7 @@ public class BasicTest {
 
   @Test
   public void login_mary() {
-    Assert.assertEquals(1, 1);
+    assertEquals(1, 1);
     System.out.println("======================");
     System.out.println("======================");
     System.out.println("======================");
@@ -71,5 +81,14 @@ public class BasicTest {
     Note note = new Note();
     note.setText("TESTING FROM ARQUILLIAN WITH EM");
     noteService2.persist(note);
+  }
+
+  @Test
+  public void givenChunk_thenBatch_completesWithSuccess() throws Exception {
+    JobOperator jobOperator = BatchRuntime.getJobOperator();
+    Long executionId = jobOperator.start("simpleChunk", new Properties());
+    JobExecution jobExecution = jobOperator.getJobExecution(executionId);
+    jobExecution = BatchTestHelper.keepTestAlive(jobExecution);
+    assertEquals(jobExecution.getBatchStatus(), BatchStatus.COMPLETED);
   }
 }
